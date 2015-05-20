9a11f09b8e106d170a05472cf7c56de581ce5358
Doonan <jdoonan44@gmail.com>
2015-05-19 22:21:53 -0400

    Add readme, along with more features and bugfixes to script

---
+++ README.md
@@ -0,0 +1,12 @@
+Thank you for checking out my project, 'bashlist.sh'!!!
+
+This script maintains a list for you and control of that list is done in the terminal.
+
+Options for the script can be read up on in the code itself, or by running the script with the argument -h or --help,
+
+The idea is for a user to have a terminal window open with the list on it, kind of like a to-do list.
+It is manipulated by calling the script name or an alias with whatever option you want and the list updates in that terminal window - bam!
+
+The sed command used in the script is set up to work on a mac, so no guarantees it will work on any other os.
+
+All I had to do to set up the script is add the directory the script will live in to my PATH variable and it should work.
--- bashlist.sh
+++ bashlist.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 #this script adds items to a list in a seperate text file
-conf=list.txt		#name of config file
+conf=$HOME/list.txt		#name of config file
 if [[ ! -f $conf ]]; then
 	touch $conf		#creates config file if it doesn't exist
 	echo conf file $conf created
 fi
 
+function readit {		#this is the read through list function
+	echo $USER\'s list:
+	i=1
+	while read l; do
+		echo $i - $l
+		((i++))
+	done <$conf
+}
+
 function help {		#this is the help message
+	readit
 	echo this is the 'bashlist' script help message
 	echo this script maintains a list of things for you
 	echo
@@ -14,47 +24,78 @@ function help {		#this is the help message
 	echo using -l or --list will read to you the list
 	echo using -f or --fpop will remove the first item from the list
 	echo using -b or --bpop will remove the first item from the list
+	echo using -r or --remove and a number will remove that number item from the list
+	echo using -r or --remove alone works the same as -f or --fpop
 	echo using no argument works the same as -c and --conf
 	echo any other argument is read as input and added to the bottom of the list
 	echo
-	echo the list is kept in $conf in the directory this script is located in
+	echo the list is kept in $conf
 	echo
 }
 
-if [ "$#" -gt 1 ]; then
+if [ "$#" -gt 2 ]; then
+	clear
     echo too many parameters
     help
-elif [ -z "$1" ]; then
-	while read l; do		#reading through list
-		echo $l
-	done <$conf
-	exit
-else
-	if [ "$1" = -h ] || [ "$1" = --help ]; then		#catch for help option
-		help
-	elif [ "$1" = -l ] || [ "$1" = --list ]; then		#catch for list option
-		while read l; do		#reading through list
-			echo $l
-		done <$conf
-	elif [ "$1" = -f ] || [ "$1" = --fpop ]; then		#catch for fpop option
-		if [[ -s $conf ]] ; then		#checking fo an empty list
+    exit
+elif [ "$1" = -r ] || [ "$1" = --remove ]; then		#catch for remove option
+	clear
+	if [[ -s $conf ]] ; then		#checking for an empty list
+		if [ "$#" -gt 1 ]; then
+			numlines=$(cat $conf | wc -l)
+			if [ "$2" -gt "$numlines" ]; then
+				echo that number entry does not exist
+			else
+				echo removing entry number $2
+				sed -i '' "$2"d $conf		#mac osx way to use sed
+			fi
+		else
 			line=$(head -n 1 $conf)
 			echo removing first entry, $line
 			sed -i '' '1d' $conf		#mac osx way to use sed
+		fi
+		readit
+	else
+		echo list is empty
+	fi
+	exit
+elif [ "$#" -gt 1 ]; then
+	clear
+    echo too many parameters
+    help
+    exit
+elif [ -z "$1" ]; then
+	clear
+	readit
+	exit
+else
+	clear
+	if [ "$1" = -h ] || [ "$1" = --help ]; then		#catch for help option
+		help
+	elif [ "$1" = -l ] || [ "$1" = --list ]; then		#catch for list option
+		readit
+	elif [ "$1" = -f ] || [ "$1" = --fpop ]; then		#catch for fpop option
+		if [[ -s $conf ]] ; then		#checking for an empty list
+			line=$(head -n 1 $conf)
+			echo removing first entry, $line
+			sed -i '' '1d' $conf		#mac osx way to use sed
+			readit
 		else
 			echo list is empty
 		fi;
 	elif [ "$1" = -b ] || [ "$1" = --bpop ]; then		#catch for bpop option
-		if [[ -s $conf ]] ; then		#checking fo an empty list
+		if [[ -s $conf ]] ; then		#checking for an empty list
 			line=$(tail -1 $conf | head -1)
 			echo removing last entry, $line
 			sed -i '' '$d' $conf		#mac osx way to use sed
+			readit
 		else
 			echo config list is empty
 		fi;
 	else
 		echo "$1" >> $conf		#adding argument to list
 		echo "$1" added to conf file
-	exit
+		readit
 	fi
+	exit
 fi
